6533315debc74c757bc227e782454579c2f0a236
SaiAshish <saiashish3760@gmail.com>
2023-01-21 03:42:41 +0530

    Initial Commit

---
+++ ELKStackLogging/src/main/java/com/spring/ELKStackLogging/ElkStackLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ELKStackLogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkStackLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkStackLoggingApplication.class, args);
+	}
+
+}
